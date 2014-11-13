71d5cb68ed33eea281dfc2018e9b0448d16d9826
Chihiro Akiba <chihiroakiba@Chihiro-no-MacBook-Pro.local>
2014-11-13 23:22:21 +0900

    first commit

---
+++ app/src/main/java/com/tuqulore/memorablelist/MyActivity.java
@@ -0,0 +1,54 @@
+package com.tuqulore.memorablelist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MyActivity extends Activity {
+
+    Button mNewBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        View.OnClickListener mNewClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                startActivityForResult(intent, 0);
+            }
+        };
+        mNewBtn = (Button)findViewById(R.id.btn_new);
+        mNewBtn.setOnClickListener(mNewClickListener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
