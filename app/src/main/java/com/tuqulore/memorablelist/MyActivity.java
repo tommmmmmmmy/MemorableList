1480e3a5c6e63bc274c6d7b9d3508a7f07083bb2
hrk fkm <fkm@fkmsoft.jp>
2014-11-14 00:11:03 +0900

    Convert project to appcompat-v7

--- app/src/main/java/com/tuqulore/memorablelist/MyActivity.java
+++ app/src/main/java/com/tuqulore/memorablelist/MyActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends ActionBarActivity {
 
     Button mNewBtn;
 
