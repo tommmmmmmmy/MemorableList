5b2af44ca90f969e0ab915c1a6685676c2eec99f
hrk fkm <fkm@fkmsoft.jp>
2014-11-14 19:52:04 +0900

    ライブラリ周りをいろいろ設定

--- app/src/main/java/com/tuqulore/memorablelist/MyActivity.java
+++ app/src/main/java/com/tuqulore/memorablelist/MyActivity.java
@@ -1,15 +1,11 @@
 package com.tuqulore.memorablelist;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+
+import com.tuqulore.memorablelist.page.main.impl.MyFragment;
+
+import jp.fkmsoft.android.framework.util.FragmentUtils;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -17,29 +13,13 @@ public class MyActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_my);
 
-        LayoutInflater inflater = LayoutInflater.from(this);
-        View root = inflater.inflate(R.layout.activity_my, null);
-        setContentView(root);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_archive) {
-            return true;
+        // 初回起動時のみ処理する
+        if (savedInstanceState == null) {
+            // R.id.containerの位置に、MyFragmentを張る
+            FragmentUtils.toNextFragment(getSupportFragmentManager(), R.id.container, new MyFragment(), false);
         }
-        return super.onOptionsItemSelected(item);
+
     }
 }
---
+++ app/src/main/java/com/tuqulore/memorablelist/page/main/impl/MainAppImpl.java
@@ -0,0 +1,16 @@
+package com.tuqulore.memorablelist.page.main.impl;
+
+import com.tuqulore.memorablelist.page.main.MainApp;
+import com.tuqulore.memorablelist.page.main.MainUI;
+
+import jp.fkmsoft.android.framework.UIHolder;
+import jp.fkmsoft.android.framework.base.BaseApp;
+
+/**
+ * Implementation
+ */
+class MainAppImpl extends BaseApp<MainUI> implements MainApp {
+    MainAppImpl(UIHolder<MainUI> uiHolder) {
+        super(uiHolder);
+    }
+}
