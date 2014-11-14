8a6a44bf037ad9d3ea086a3fd59550d0e5460cee
Chihiro Akiba <chihiroakiba@Chihiro-no-MacBook-Pro.local>
2014-11-15 00:17:20 +0900

    オーバーフローのArchivesを押すとArchiveFragmentに遷移、戻るボタンで終了するｗ

--- app/src/main/java/com/tuqulore/memorablelist/page/main/impl/ArchiveFragment.java
+++ app/src/main/java/com/tuqulore/memorablelist/page/main/impl/ArchiveFragment.java
@@ -1,10 +1,20 @@
 package com.tuqulore.memorablelist.page.main.impl;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tuqulore.memorablelist.R;
 
 /**
  * Created by chihiroakiba on 2014/11/15.
  */
 public class ArchiveFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.archive_my, container, false);
 
+        return root;
+    }
 }
